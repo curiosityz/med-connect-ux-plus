8c45768786f22268a9f97c46c0a1c13a368c1fd5
curiosityz <marketerzach@gmail.com>
2025-06-01 14:39:49 -0400

    refactor(finder): update import statement for consistency

--- src/app/finder/page.tsx
+++ src/app/finder/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
